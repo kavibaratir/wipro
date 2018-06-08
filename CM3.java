52676cf4ae3ad78cdd09783ac4cbf18c2faca89e
kavibaratir <32194416+kavibaratir@users.noreply.github.com>
2018-06-07 21:49:29 -0700

    Add files via upload

---
+++ CM3.java
@@ -0,0 +1,10 @@
+package wiproassingment1;
+
+public class CM3 {
+ public static void main(String[] args) {
+	int i=Integer.parseInt(args[0]);
+	int j=Integer.parseInt(args[1]);
+	int a=i+j;
+	System.out.println("the sum of "+args[0]+" and "+args[1]+" is "+a );
+}
+}
